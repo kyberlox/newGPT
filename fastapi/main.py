5f00ffc31588522d46d2fdddf6c3de36664c9323
kyberlox <alexxx.timofeev.00@gmail.com>
2025-07-04 21:33:50 +0400

    Ввожу в эксплоатацию новый стек чата GPT на корпоративном сайте

--- fastapi/main.py
+++ fastapi/main.py
@@ -43,8 +43,8 @@ app.add_middleware(
 
 @app.post("/")
 def root(data = Body()):
-    #params = json.loads(data.decode('UTF-8'))
-    params = data
+    params = json.loads(data.decode('UTF-8'))
+    #params = data
     print(params)
     output = client.chat.completions.create(
         model = params["model"],
