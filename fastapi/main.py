6d0606d32a14d4c92743817fdf889dfc7a808b15
kyberlox <64195832+kyberlox@users.noreply.github.com>
2025-11-25 10:53:32 +0300

    отладка генерации

--- fastapi/main.py
+++ fastapi/main.py
@@ -313,7 +313,7 @@ async def generate_image(data=Body()):
 
     except openai.BadRequestError as e:
         raise HTTPException(status_code=400, detail=f"Неверный запрос: {str(e)}")
-        print(response)
+        print(e, response)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ошибка: {str(e)}")
 
